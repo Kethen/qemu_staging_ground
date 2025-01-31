550598e266004426b3cb24374a35fd42d71d38e3
Katharine Chui <kwchuiaa@connect.ust.hk>
2025-01-31 13:50:05 +0100

    sync

--- boot_stage_2.sh
+++ boot_stage_2.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #PLATFORM="-cpu Skylake-Client-v4"
 PLATFORM="-cpu host"
-PLATFORM="$PLATFORM -enable-kvm -smp sockets=1,cores=4,threads=1 -m 4G -M q35,i8042=off"
+PLATFORM="$PLATFORM -enable-kvm -smp sockets=1,cores=4,threads=1 -m 2G -M q35,i8042=off"
 PLATFORM="$PLATFORM -global ICH9-LPC.disable_s3=1 -global ICH9-LPC.disable_s4=1"
 PLATFORM="$PLATFORM -global ICH9-LPC.acpi-pci-hotplug-with-bridge-support=off"
 
-NETWORK="-netdev user,id=eth0"
+NETWORK="-netdev user,id=eth0,hostfwd=tcp:127.0.0.1:8022-:8022,hostfwd=tcp:127.0.0.1:7777-:7777"
 NETWORK="$NETWORK -device virtio-net,netdev=eth0"
 
-STORAGE="-drive if=none,id=maindisk,format=raw,file=maindisk.img,discard=unmap -object iothread,id=iomaindisk -device virtio-blk,iothread=iomaindisk,drive=maindisk"
+STORAGE="-drive if=none,id=maindisk,format=raw,file=maindisk_15.img,discard=unmap -object iothread,id=iomaindisk -device virtio-blk,iothread=iomaindisk,drive=maindisk"
 #STORAGE="-drive if=virtio,id=maindisk,format=raw,file=maindisk.img,discard=unmap"
 
 USB="-device qemu-xhci"
 
-INPUT="-device virtio-keyboard-pci"
+INPUT="$INPUT -device virtio-mouse-pci"
 INPUT="$INPUT -device virtio-multitouch-pci"
+INPUT="$INPUT -device usb-kbd"
 #INPUT="$INPUT -device usb-mouse"
 #INPUT="$INPUT -device usb-tablet"
 if false
@@ -68,9 +69,9 @@ DISP="$DISP -vga none -device virtio-vga-gl,xres=1280,yres=800"
 
 MISC="-nodefaults"
 MISC="$MISC -name bliss,debug-threads=on"
-MISC="$MISC -rtc base=utc"
+MISC="$MISC -rtc base=utc,clock=host"
 
-QEMU="qemu-kvm"
+#QEMU="qemu-kvm"
 QEMU="./qemu/build/qemu-system-x86_64"
 
 export vblank_mode=0
@@ -89,11 +90,29 @@ then
 	$QEMU -device virtio-vga-rutabaga,help
 fi
 
-#$QEMU $PLATFORM $NETWORK $STORAGE $USB $INPUT $AUDIO $DISP $MISC -cdrom Fedora-Workstation-Live-x86_64-40-1.14.iso -boot d -L /usr/share/seabios -L /usr/share/ipxe/qemu -L /usr/share/qemu -L /usr/share/seavgabios
-$QEMU $PLATFORM $NETWORK $STORAGE $USB $INPUT $AUDIO $DISP $MISC -kernel kernel -append 'SRC=/bliss14 DATA=userdata CODEC2_LEVEL=0 FFMPEG_OMX_CODEC=1 VIRT_WIFI=1 OMX_NO_YUV420=1' -initrd initrd.img -L /usr/share/seabios -L /usr/share/ipxe/qemu -L /usr/share/qemu -L /usr/share/seavgabios
+#bash -l
+#exit 0
 
+(
+	while true
+	do
+		./battery_poll
+	done
+) 2>/dev/null 1>/dev/null &
+battery_poll_pid=$!
+
+export vblank_mode=0
+
+$QEMU $PLATFORM $NETWORK $STORAGE $USB $INPUT $AUDIO $DISP $MISC -cdrom Fedora-Workstation-Live-x86_64-40-1.14.iso -boot d -L /usr/share/seabios -L /usr/share/ipxe/qemu -L /usr/share/qemu -L /usr/share/seavgabios
+#$QEMU $PLATFORM $NETWORK $STORAGE $USB $INPUT $AUDIO $DISP $MISC -kernel kernel -append 'mitigations=off SRC=/bliss14 DATA=userdata CODEC2_LEVEL=0 FFMPEG_OMX_CODEC=1 VIRT_WIFI=1 OMX_NO_YUV420=1' -initrd initrd.img -L /usr/share/seabios -L /usr/share/ipxe/qemu -L /usr/share/qemu -L /usr/share/seavgabios
+#$QEMU $PLATFORM $NETWORK $STORAGE $USB $INPUT $AUDIO $DISP $MISC -kernel kernel_15 -append 'mitigations=off SRC=/bliss15 DATA=userdata CODEC2_LEVEL=0 FFMPEG_OMX_CODEC=1 VIRT_WIFI=1 OMX_NO_YUV420=1' -initrd initrd_15.img -L /usr/share/seabios -L /usr/share/ipxe/qemu -L /usr/share/qemu -L /usr/share/seavgabios
 
 if [ -n "$WESTON_PID" ]
 then
 	kill -9 $WESTON_PID
+fi
+
+if [ -n "$battery_poll_pid" ]
+then
+	kill $battery_poll_pid
 fi
